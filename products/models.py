c086181f76da1da38587ff84283759d493d2ac7f
Mohammed-Essam-ai <mo789965@gmail.com>
2026-07-02 14:53:51 +0300

    Updating Product Model

--- products/models.py
+++ products/models.py
@@ -20,7 +20,9 @@ class Product(models.Model):
         related_name="products"
     )
 
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    amazon_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    noon_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     rating = models.DecimalField(
         max_digits=2,
@@ -32,11 +34,14 @@ class Product(models.Model):
 
     image_path = models.CharField(max_length=500, blank=True)
     
-    product_url = models.URLField(max_length=1000)
+    amazon_product_url = models.URLField(max_length=1000)
+
+    noon_product_url = models.URLField(max_length=1000)
 
     created_at = models.DateTimeField(auto_now_add=True)
 
     updated_at = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return self.name
